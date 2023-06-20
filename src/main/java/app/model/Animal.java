59855c7eb4d09e7ad323c82bd322f694ae0e3522
vokhu <khukharev@gmail.com>
2023-06-20 21:43:02 +0300

    20/06

--- src/main/java/app/model/Animal.java
+++ src/main/java/app/model/Animal.java
@@ -1,4 +1,11 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+
+
 public abstract class Animal {
+
 }
